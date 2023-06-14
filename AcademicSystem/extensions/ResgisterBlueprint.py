2b3d8afe849c2a0e5bd7b07b27d9a0e4a030e5b0
Rezende-Fabio <rezendefabio337@gmail.com>
2023-06-14 19:38:06 -0300

    correcting blueprint criaBanco

--- AcademicSystem/extensions/ResgisterBlueprint.py
+++ AcademicSystem/extensions/ResgisterBlueprint.py
@@ -1,4 +1,4 @@
-from AcademicSystem.route.CriaBanco import crairBancoBlueprint
+from AcademicSystem.route.CriaBanco import criarBancoBlueprint
 
 def resgister_blueprint(app):
-    app.register_blueprint(crairBancoBlueprint)
+    app.register_blueprint(criarBancoBlueprint)
