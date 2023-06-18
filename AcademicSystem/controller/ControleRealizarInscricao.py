be6811d9d7335467ee790806ff1c3671afc2af60
Rezende-Fabio <rezendefabio337@gmail.com>
2023-06-18 15:05:30 -0300

    resolve return instance Disciplina

--- AcademicSystem/controller/ControleRealizarInscricao.py
+++ AcademicSystem/controller/ControleRealizarInscricao.py
@@ -1,12 +1,12 @@
-#from ..model.entity.OfertaDisciplina import OfertaDisciplina
+from ..model.entity.OfertaDisciplina import OfertaDisciplina
 
 class ControleRealizarInscricao:
 
-    def filtrarOfertas(self) -> int:
-        return 15
+    def filtrarOfertas(self) -> list[OfertaDisciplina]:
+        pass
 
-    def confirmarInscricao(self) -> int:
-        return 10
+    def confirmarInscricao(self) -> None:
+        pass
 
     def adicionarListaEspera(self) -> None:
         pass
